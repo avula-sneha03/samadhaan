fff81d74e9d41106d5cbffaf85c7b762d5c5347f
Lalit Kumar Sharma <lalitsharma.2001jsr@gmail.com>
2025-01-27 23:30:33 +0530

    added-all-ques-page

---
+++ frontend/src/Components/Discussions/Allques.jsx
@@ -0,0 +1,8 @@
+const Allques = ()=>{
+return(
+    <div>
+        Here are the list of all questions
+    </div>
+)
+}
+export default Allques
--- frontend/src/Components/Discussions/Discussions.jsx
+++ frontend/src/Components/Discussions/Discussions.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react"
 import Navbar from "../Navbar"
+import Allques from "./Allques";
 
 const Discussions =()=>{
+    const activetab=useState("");
+
+    const renderedcontent = ()=>{
+        switch(activetab){
+         case "allexp":
+         return <Allques/>
+         default:
+            return <Allques/>
+        }
+        
+        
+    }
 return(
     <>
     <Navbar/>
     <div>Discussions Page</div>
+    <div>{renderedcontent}</div>
     </>
 )
 }
